5c1842df260798db36464ec0807cfcdcba84b69a
ProDoubleG <jasonmoon106@gmail.com>
2026-02-10 18:18:06 +0900

    Fix port conflict

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -5,7 +6,9 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     # templates 폴더 내의 index.html을 보여줌
-    return render_template('index.html')
+    # 환경변수 'Page_Title'을 읽어오고, 없으면 기본값 'CICD Practice' 사용
+    page_title = os.getenv("Page_Title", "CICD Practice")
+    return render_template('index.html', title=page_title)
 
 @app.route('/core')
 def core_function():
