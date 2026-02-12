c94bb790cae4cdef3650df07b6b4d032a0046c7d
ProDoubleG <jasonmoon106@gmail.com>
2026-02-12 16:10:19 +0900

    Add : New feature corrupting core function

--- app/main.py
+++ app/main.py
@@ -9,18 +9,29 @@ def home():
     # 환경변수 'Page_Title'을 읽어오고, 없으면 기본값 'CICD Practice' 사용
     page_title = os.getenv("Page_Title", "CICD Practice")
     return render_template('index.html', title=page_title)
+core_response = "Success"
 
 @app.route('/core')
 def core_function():
-    return "Success"
+    return core_response
 
 @app.route('/legacy')
 def legacy_feature():
     return "Response A"
 
+# #------------- Scenario A ---------------------
+# @app.route('/new-feature')
+# def new_feature():
+#     return "Fail", 500
+# #----------------------------------------------
+
+# #------------- Scenario B ---------------------
 @app.route('/new-feature')
 def new_feature():
-    return "Fail", 500
+    global core_response
+    core_response = "Fail" 
+    return "Response B"
+# -----------------------------------------------
 
 if __name__ == '__main__':
     # 5000번 포트에서 실행, 외부 접속 허용(0.0.0.0)
